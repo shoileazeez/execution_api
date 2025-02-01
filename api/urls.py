fc827c22d5dbcabe0f2985e1201b04f601520a89
shoile azeez <shoabdulazeez@gmail.com>
2025-02-01 01:48:24 +0100

    changes

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("execute/", views.execute_code, name="execution_api"),
-    path("execute_result/", views.get_task_result, name="result"),
+    path("execute_result/<str:task_id>", views.get_task_result, name="result"),
     path("health/", views.health_check, name="health_check"),
 ]
